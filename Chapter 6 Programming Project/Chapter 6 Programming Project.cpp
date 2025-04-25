4d95a0ae8292af8fae617ebd118981265780d86b
Jesse zheng <BessiieBoi@outlook.com>
2025-04-25 18:04:29 -0400

    Version1

--- Chapter 6 Programming Project/Chapter 6 Programming Project.cpp
+++ Chapter 6 Programming Project/Chapter 6 Programming Project.cpp
@@ -4,87 +4,91 @@
 
 #include <iostream>
 #include <string>
-#include <limits>
-
-using namespace std;
+#include <vector>
 
 /*
-    Function: getRegInfo
-    Purpose : Gets the region name and number of accidents from user input.
-    Parameters:
-        - regionName: the name of the region
-        - numAccidents: the number of reported accidents (must be >= 0)
-    Preconditions:
-        - numAccidents must be 0 or greater
-    Postconditions:
-        - regionName and validated numAccidents are set usig user input
+   Function: getRegInfo
+   Purpose : Prompts the user to enter the number of automobile accidents reported for a specific region and validates the input.
+   Parameters:
+	  - regionName (std::string): The name of the region for which accident data is being requested.
+	  - numAccidents (int&): Reference to an integer where the validated accident count will be stored.
+   Returns :
+	  - None (void). The accident count is returned using pass by reference parameter.
+   Preconditions:
+	  - regionName must be a valid region string ("North", "South", etc.).
+   Postconditions:
+	  - numAccidents will contain a non-negative integer value entered by the user.
 */
-void getRegInfo(string& regionName, int& numAccidents) {
-    cout << "Enter region name: ";
-    cin >> regionName;
+void getRegInfo(std::string regionName, int& numAccidents) {
+	do {
+		std::cout << "Enter number of accidents in " << regionName << " (>= 0): ";
+		std::cin >> numAccidents;
 
-    do {
-        cout << "Enter number of accidents in " << regionName << " (>= 0): ";
-        cin >> numAccidents;
-
-        if (cin.fail() || numAccidents < 0) {
-            cout << "Invalid input. Number of accidents must be 0 or greater.\n";
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        }
-    } while (numAccidents < 0);
+		if (numAccidents < 0) {
+			std::cout << "Invalid input. Number of accidents must be 0 or greater. \n";
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+	} while (numAccidents < 0);
 }
 
 /*
-    Function: isLower
-    Purpose : Checks if the first value is less than the second
-    Parameters:
-        - int value1: first number to compare
-        - int value2: second number to compare
-    Returns:
-        - true if value1 < value2, otherwise false
+   Function: isLower
+   Purpose : Compares two integers and returns true if the first is less than the second.
+   Parameters:
+	  - value1 (int): The first value to compare.
+	  - value2 (int): The second value to compare.
+   Returns :
+	  - bool: true if value1 < value2; false otherwise.
+   Preconditions:
+	  - None.
+   Postconditions:
+	  - Returns a boolean indicating whether value1 is less than value2.
 */
 bool isLower(int value1, int value2) {
-    return value1 < value2;
+	return value1 < value2;
 }
 
 /*
-    Function: showLowest
-    Purpose : Displays the region with the fewest reported accidents
-    Parameters:
-        - string regionName: the name of the region
-        - int numAccidents: number of reported accidents in that region
+   Function: showLowest
+   Purpose : Displays the name of the region with the lowest number of reported automobile accidents.
+   Parameters:
+      - regionName (const std::string&): Name of the region with the fewest accidents.
+      - numAccidents (int): Number of accidents in that region.
+   Returns :
+      - None (void).
+   Preconditions:
+      - numAccidents should be a non-negative integer.
+   Postconditions:
+      - Outputs a formatted message to the console showing the safest region and its accident count.
 */
-void showLowest(string regionName, int numAccidents) {
-    cout << "\nThe safest region is " << regionName
-        << " with " << numAccidents << " reported accidents." << endl;
+void showLowest(const std::string& regionName, int numAccidents) {
+	std::cout << "\nThe safest region is " << regionName
+		<< " with " << numAccidents << " reported accidents." << std::endl;
 }
 
 /*
-    Function: main
-    Purpose : Controls program flow to determine the safest region
+   Function: main
+   Purpose : Collects accident data for five regions and determines which had the fewest accidents.
 */
 int main() {
-    const int NUM_REGIONS = 5;
-    string regions[NUM_REGIONS] = { "North", "South", "East", "West", "Central" };
+	std::vector<std::string> regionNames = { "North", "South", "East", "West", "Central" };
+	std::vector<int> numAccidents(regionNames.size());
 
-    string safestRegion;
-    int lowestAccidents = numeric_limits<int>::max();
+	std::string safestRegion;
+	int lowestAccidents;
 
-    for (int i = 0; i < NUM_REGIONS; i++) {
-        string currentRegion;
-        int currentAccidents;
+	for (int i = 0; i < regionNames.size(); ++i) {
+		getRegInfo(regionNames[i], numAccidents[i]);
 
-        currentRegion = regions[i];
-        getRegInfo(currentRegion, currentAccidents);
+		// Initialize with the first region
+		if (i == 0 || isLower(numAccidents[i], lowestAccidents)) {
+			lowestAccidents = numAccidents[i];
+			safestRegion = regionNames[i];
+		}
+	}
 
-        if (isLower(currentAccidents, lowestAccidents)) {
-            lowestAccidents = currentAccidents;
-            safestRegion = currentRegion;
-        }
-    }
+	showLowest(safestRegion, lowestAccidents);
 
-    showLowest(safestRegion, lowestAccidents);
-
-    return 0;
+	return 0;
 }
